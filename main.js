ddf869bd30253cb4dfb3ad7dd33e2986d33a4fd9
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-26 16:04:46 -0500

    Fixed all bugs

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-let continue = true
+let cont = true
+let goodbyes = 0
+let userInput = window.prompt('HEY, KID!')
 
-while ( continue ) {
+while ( cont === true ) {
 
-    let goodbyes = 0
-    let userInput = prompt()
-
-    if ( userInput = "" ) {
-        alert("WHAT!?")
-    }
-    else if ( userinput.toUppercase != statement ) {
-        alert("SPEAK UP, KID!")
-    }
-    else if ( statement.toUppercase == statement ) {
-        alert('NO, NOT SINCE 1946!')
-    }
-    else if ( userinput == "GOODBYE!" ) {
-        goodbyes + 1
-
-        if ( goodbyes == 1 ) {
-            alert("LEAVING SO SOON?')
+    if ( userInput === "" ) {
+        userInput=window.prompt("WHAT!?")
+    }else {
+        if ( userInput !== userInput.toUpperCase() ) {
+            userInput=window.prompt("SPEAK UP, KID!")
         }
-        else if ( goodbyes == 2 ) {
-            alert("LATER, SKATER!")
-            continue = false
-        }
-    }
+        else if ( userInput === "GOODBYE!" ) {
+            if ( goodbyes < 1 ) {
+                userInput=window.prompt("LEAVING SO SOON?")
+                goodbyes++
+            }
+            else {
+                alert("LATER, SKATER!")
+                cont = false
+            }
+        } else {
+            userInput=window.prompt('NO, NOT SINCE 1946!')
+        } 
+    } 
+    
 }
